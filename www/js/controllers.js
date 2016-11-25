01c22243502e80dadc016dd62fde3fa282eff982
Pierce <piercejord@gmail.com>
2016-11-25 16:19:03 +0000

    95% Complete, Some bugs to be fixed - PJ

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,55 +2,77 @@ angular.module('PJAKApp.controllers', [])
 .controller('NasaController', function($scope, settingsService, NasaInfoService) {
 
   $scope.$on('$ionicView.beforeEnter', function() {
-    $scope.days = settingsService.getDays();
-    $scope.dates = NasaInfoService.getDate($scope.days);
-    NasaInfoService.getPicOfDay($scope.dates);
+    $scope.NasaInfoArr = [];
+    var days = settingsService.getDays();
+    var dates = NasaInfoService.getDate(days);
+    var urlArr = NasaInfoService.getURLArr(dates);
+    NasaInfoService.getPicOfDay(urlArr, responseOK, responseNOK);
+
+    function responseOK(responseData) {
+      $scope.NasaInfoArr.push(responseData);
+      for (var i = 0; i<$scope.NasaInfoArr.length; i++){
+        if ($scope.NasaInfoArr[i].media_type === "image") {
+          $scope.img = true;
+          $scope.video = false;
+        } else {
+          $scope.img = false;
+          $scope.video = true;
+        }
+    }
+  }
+
+    function responseNOK(errorStatus, errorStatusText) {
+      alert(errorStatus + " " + errorStatusText);
+    }
   });
 })
 
 .controller('SettingsCTRL', function ($scope, settingsService){
 
   $scope.noOfDays = function(days) {
-      settingsService.setDays(days);
-    }
-    $scope.$on('days', function (){
-      $scope.days = settingsService.getDays();
-    });
-    /*var selectedValue = value.value;
-    console.log(selectedValue);
-    //var oneDay = 0;
-    var oneDay = 0;
-    for (var i=0; i<selectedValue; i++){
-      var j=1;
-      var dateObj = function(date){
-        $scope.dateArr.push(this);
-        var temp = new Date(date);
-        this.date = temp.toISOString().split('T')[0];
-      }
-      new dateObj(new Date());
-      var today = new Date($scope.dateArr[i].date);
-      var yesterday = new Date(today - oneDay);
-      //$scope.nasaDate = yesterday.toISOString().split('T')[0];
-      oneDay = 1000 * 60 * 60 * 24;
-      //this.nasaDateArr.push(nasaDate);
-      //console.log(this.nasaDateArr);
-      console.log($scope.dateArr[i].date);
-    }*/
+    settingsService.setDays(days);
+  }
+  $scope.$on('days', function (){
+    $scope.days = settingsService.getDays();
+  });
 
-    //SettingsService.setDate(2)
-    /*var days = value.value;
-    console.log(days);
-    //$scope.$on('$ionicView.beforeEnter', function () {
-      if (days === 1) {
-        SettingsService.setDate(++days);
-        $scope.data.dateMod = "1";
-      } else if (days === 7) {
-        SettingsService.setDate(++days);
-        $scope.data.dateMod = "7";
-      } else if (days === 14){
-        SettingsService.setDate(++days);
-        $scope.data.dateMod = "14";
-      }
-      //console.log($scope.data.clientSide);
-    //})*/
-  })
+  /*
+*******Old Code**********
+  var selectedValue = value.value;
+  console.log(selectedValue);
+  //var oneDay = 0;
+  var oneDay = 0;
+  for (var i=0; i<selectedValue; i++){
+  var j=1;
+  var dateObj = function(date){
+  $scope.dateArr.push(this);
+  var temp = new Date(date);
+  this.date = temp.toISOString().split('T')[0];
+}
+new dateObj(new Date());
+var today = new Date($scope.dateArr[i].date);
+var yesterday = new Date(today - oneDay);
+//$scope.nasaDate = yesterday.toISOString().split('T')[0];
+oneDay = 1000 * 60 * 60 * 24;
+//this.nasaDateArr.push(nasaDate);
+//console.log(this.nasaDateArr);
+console.log($scope.dateArr[i].date);
+}*/
+
+//SettingsService.setDate(2)
+/*var days = value.value;
+console.log(days);
+//$scope.$on('$ionicView.beforeEnter', function () {
+if (days === 1) {
+SettingsService.setDate(++days);
+$scope.data.dateMod = "1";
+} else if (days === 7) {
+SettingsService.setDate(++days);
+$scope.data.dateMod = "7";
+} else if (days === 14){
+SettingsService.setDate(++days);
+$scope.data.dateMod = "14";
+}
+//console.log($scope.data.clientSide);
+//})*/
+})
--- www/js/services.js
+++ www/js/services.js
@@ -1,11 +1,27 @@
 angular.module('PJAKApp.services', [])
 .service('NasaInfoService', function($http, $rootScope) {
-  this.getPicOfDay = function(dates) {
-    console.log(dates);
+  this.getPicOfDay = function(dates, callbackOK, callbackNOK) {
+    var searchURLArr = dates;
+    for (var i=0; i<searchURLArr.length; i++) {
+      $http.get(searchURLArr[i]).then(funOK, funNOK);
+
+      function funOK(response) {
+        callbackOK(response.data);
+      };
+
+      function funNOK(response) {
+        callbackNOK(response.status, response.statusText);
+      };
+    }
+
+  }
+  this.getURLArr = function(dates) {
+    var searchURLArr = [];
     for (var i=0; i<dates.length; i++) {
       searchURL = "https://api.nasa.gov/planetary/apod?api_key=I8nPGWKTzor2AOHDZK2bVANlS1X6EiNMVHWGh4Xe&date=" + dates[i];
-      console.log(searchURL);
+      searchURLArr.push(searchURL);
     }
+    return searchURLArr;
   }
   this.getDate = function(days){
     var dateArr = [];
@@ -28,7 +44,6 @@ angular.module('PJAKApp.services', [])
 .service('settingsService', function($rootScope){
 
   this.setDays = function(days) {
-    console.log(days);
     this.days = days;
     $rootScope.$broadcast('days');
   }
