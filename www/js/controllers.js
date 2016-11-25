2e59a12408853fe1776a9483b7ca60c1ff6dba59
Pierce <piercejord@gmail.com>
2016-11-25 12:01:10 +0000

    Complete Overhall of Code - PJ

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,33 +1,56 @@
 angular.module('PJAKApp.controllers', [])
-.controller('NasaController', function($scope, NasaInfoService) {
-  $scope.NasaInfoArr = [];
-  $scope.nasaInfo = NasaInfoService.getPicOfDay(function(nasaInfo) {
-    $scope.nasaInfo = nasaInfo;
-    $scope.NasaInfoArr.push(nasaInfo);
-  }, function(errorStatus, errorStatusText) {
-    $scope.errorMsg = "ERROR: ";
-    $scope.errorStatus = errorStatus;
-    $scope.errorStatusText = errorStatusText;
+.controller('NasaController', function($scope, settingsService, NasaInfoService) {
+
+  $scope.$on('$ionicView.beforeEnter', function() {
+    $scope.days = settingsService.getDays();
+    $scope.dates = NasaInfoService.getDate($scope.days);
+    NasaInfoService.getPicOfDay($scope.dates);
   });
 })
 
-.controller('SettingsCTRL', function ($scope){
-  $scope.clientSideList = [
-    { text: "Today", value: "1" },
-    { text: "Last 7 Days", value: "7" },
-    { text: "Last 14 Days", value: "14" }
-  ];
+.controller('SettingsCTRL', function ($scope, settingsService){
 
-  $scope.data = {
-    clientSide: '1'
-  };
-  $scope.noOfDays = function() {
-    if ($scope.data.clientSide === "1") {
-      console.log("1");
-    } else if ($scope.data.clientSide === "7") {
-      console.log("7");
-    } else {
-      console.log("14");
+  $scope.noOfDays = function(days) {
+      settingsService.setDays(days);
     }
-  }
-})
+    $scope.$on('days', function (){
+      $scope.days = settingsService.getDays();
+    });
+    /*var selectedValue = value.value;
+    console.log(selectedValue);
+    //var oneDay = 0;
+    var oneDay = 0;
+    for (var i=0; i<selectedValue; i++){
+      var j=1;
+      var dateObj = function(date){
+        $scope.dateArr.push(this);
+        var temp = new Date(date);
+        this.date = temp.toISOString().split('T')[0];
+      }
+      new dateObj(new Date());
+      var today = new Date($scope.dateArr[i].date);
+      var yesterday = new Date(today - oneDay);
+      //$scope.nasaDate = yesterday.toISOString().split('T')[0];
+      oneDay = 1000 * 60 * 60 * 24;
+      //this.nasaDateArr.push(nasaDate);
+      //console.log(this.nasaDateArr);
+      console.log($scope.dateArr[i].date);
+    }*/
+
+    //SettingsService.setDate(2)
+    /*var days = value.value;
+    console.log(days);
+    //$scope.$on('$ionicView.beforeEnter', function () {
+      if (days === 1) {
+        SettingsService.setDate(++days);
+        $scope.data.dateMod = "1";
+      } else if (days === 7) {
+        SettingsService.setDate(++days);
+        $scope.data.dateMod = "7";
+      } else if (days === 14){
+        SettingsService.setDate(++days);
+        $scope.data.dateMod = "14";
+      }
+      //console.log($scope.data.clientSide);
+    //})*/
+  })
--- www/js/services.js
+++ www/js/services.js
@@ -1,32 +1,38 @@
 angular.module('PJAKApp.services', [])
-.service('NasaInfoService', function($http) {
-  this.getPicOfDay = function(callbackOK, callbackNOK) {
-    for (var i=0; i < 7; i++) {
-      var d = new Date();
-      d.setDate(d.getDate() - i);
-      var nasaMonth = d.getMonth() +1;
-      var nasaDate = "" + d.getFullYear() + "";
-      nasaDate += "-" + nasaMonth + "-";
-      nasaDate += "" + d.getDate() + "";
-      var searchURL = "https://api.nasa.gov/planetary/apod?api_key=I8nPGWKTzor2AOHDZK2bVANlS1X6EiNMVHWGh4Xe&date=" + nasaDate;
-      console.log(nasaDate);
-      $http.get(searchURL).then(funOK, funNOK);
-
-      function funOK(response) {
-        callbackOK(response.data);
-      };
-
-      function funNOK(response) {
-        callbackNOK(response.status, response.statusText);
-      };
-
+.service('NasaInfoService', function($http, $rootScope) {
+  this.getPicOfDay = function(dates) {
+    console.log(dates);
+    for (var i=0; i<dates.length; i++) {
+      searchURL = "https://api.nasa.gov/planetary/apod?api_key=I8nPGWKTzor2AOHDZK2bVANlS1X6EiNMVHWGh4Xe&date=" + dates[i];
+      console.log(searchURL);
     }
   }
+  this.getDate = function(days){
+    var dateArr = [];
+    for (var i=0; i<days; i++){
+      var date = new Date();
+      if (i === 0) {
+        var oneDay = 0;
+      } else {
+        var oneDay = (1000 * 60 * 60 *24) * (i);
+      }
+
+      var numDays = new Date(date - oneDay);
+      var dates = numDays.toISOString().split('T')[0];
+      dateArr.push(dates);
+    }
+    return dateArr;
+  }
 })
-.service('DateService', function(){
-  this.getDate = function(date) {
-    var newDate = new Date(date);
-    //var dateMonth = date.getMonth();
-    return newDate;
+
+.service('settingsService', function($rootScope){
+
+  this.setDays = function(days) {
+    console.log(days);
+    this.days = days;
+    $rootScope.$broadcast('days');
+  }
+  this.getDays = function(){
+    return this.days;
   }
 })
